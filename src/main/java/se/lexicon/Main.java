e44d32549a0c9ce77b9d7919a8159e3ed4d8a921
Carlsson111 <Linusjaa1@gmail.com>
2025-06-03 09:37:53 +0200

    Part 2 down

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -3,11 +3,12 @@ package se.lexicon;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import se.lexicon.Config.ComponentScanConfig;
 import se.lexicon.data_access.StudentDao;
+import se.lexicon.util.UserInputService;
 
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(ComponentScanConfig.class);
-        StudentDao studentDao = context.getBean(StudentDao.class);
+        UserInputService userInputService = context.getBean(UserInputService.class);
     }
 }
